d8ecb62ceeee308bd51ae6835e5598ef7beff79c
nghiemIUH <vannghiem848@gmail.com>
2022-10-21 15:03:23 +0700

    fix error webpack[remove libs webpack conflict]

--- App.jsx
+++ App.jsx
@@ -5,16 +5,16 @@ import { Provider } from "react-redux";
 import { store } from "./src/redux/store";
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <StatusBar />
-      <Index />
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <StatusBar />
+            <Index />
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
