c6947dead78f6cd4f1ac3e6b92924bdb3a039a81
SMinhChau <minhchaunguyenmtp@gmail.com>
2022-11-18 21:27:10 +0700

    add friends addgroup

---
+++ src/views/components/Model/group/AddFriendGroup.jsx
@@ -0,0 +1,91 @@
+import { Image, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { View } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import axios from "axios";
+import { URL, AvatarDefault } from "../../../../utils/constant";
+import { getToken } from "../../../../utils/function";
+import { useState } from "react";
+import { bgColor, bg_item_frient, headerBar } from "../../../../utils/color";
+
+function AddFriendGroup({ name, avatar }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={() => setChecked(true)}>
+      {checked ? (
+        <Ionicons
+          style={{ marginLeft: 10 }}
+          name={"radio-button-off-outline"}
+          size={25}
+          color="#6BC185"
+        />
+      ) : (
+        <Ionicons
+          style={{ marginLeft: 10 }}
+          name={"md-radio-button-on-outline"}
+          size={25}
+          color="#6BC185"
+        />
+      )}
+
+      <View style={styles.container__Left}>
+        {avatar ? (
+          <Image source={{ uri: avatar }} style={styles.imgMessage} />
+        ) : (
+          <Image source={{ uri: AvatarDefault }} style={styles.imgMessage} />
+        )}
+        <Text style={styles.container_name}>{name}</Text>
+      </View>
+
+      <View style={styles.container__Right}></View>
+    </TouchableOpacity>
+  );
+}
+
+export default AddFriendGroup;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 70,
+    marginVertical: 5,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomWidth: 3,
+    borderColor: `${bg_item_frient}`,
+  },
+  imgMessage: {
+    height: 45,
+    width: 45,
+    borderRadius: 50,
+    marginHorizontal: 15,
+  },
+  container_name: {
+    fontSize: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#000",
+  },
+  container__Left: {
+    width: "80%",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  container__Right: {
+    width: "20%",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    paddingRight: 10,
+    color: "#068FF",
+  },
+  contentFlatList: {
+    flex: 1,
+  },
+  icon: {
+    color: "blue",
+    paddingHorizontal: 10,
+  },
+});
